b5612d6126173d2414158420358eb86cc801620d
Clayton Monfils <120623084+ClaytonMonfils@users.noreply.github.com>
2025-04-19 21:43:12 -0400

    Possible Framework and Option 1 Pretty Much Done

--- FinalProject.java
+++ FinalProject.java
@@ -4,13 +4,143 @@
 - (Optional) Any other notes for the TA
 */
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class FinalProject {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		boolean exitProgram = false, duplicateID = false;
+		String option, fullNameInput, idInput, departmentInput, rankInput, statusInput;
+		int i;
+		Scanner inputScanner = new Scanner (System.in);
+		ArrayList<Person> PersonCollection = new ArrayList<Person>(100);
+		
+		System.out.println("\t\t\t\tWelcome to the Personnel Management System");
+		
+		while (exitProgram == false) {
+			System.out.println("Choose one of the options:");
+			System.out.println("1- Enter the information of a faculty");
+			System.out.println("2- Enter the information of a student");
+			System.out.println("3- Print tuition invoice for a student");
+			System.out.println("4- Print faculty information");
+			System.out.println("5- Enter the information of a staff member");
+			System.out.println("6- Print the information of a staff member");
+			System.out.println("7- Delete a person");
+			System.out.println("8- Exit Program\n\n");
+			System.out.print("Enter your selection: ");
+			
+			option = inputScanner.nextLine();
+			if (option.compareTo("1") == 0) {
+				i = 0;
+				
+				// Looping until 3 invalid tries completed
+				while (i < 3) {
+					System.out.print("Enter faculty info:\nName: ");
+					fullNameInput = inputScanner.nextLine();
+					System.out.print("ID: ");
+					idInput = inputScanner.nextLine();
+					
+					// Checking for invalid id format
+					try {
+						if (idInput.length() != 6) {
+							throw new idFormatException();
+						} 
+					} catch (idFormatException e) {
+						e.printMessage();
+						i = i + 1;
+						continue;
+					}
+					try {
+						if (!idInput.subSequence(0, 1).toString().matches("[a-zA-Z]+")) {
+							throw new idFormatException();
+						}
+					} catch (idFormatException e) {
+						e.printMessage();
+						i = i + 1;
+						continue;
+					}
+					try {
+						if (!idInput.subSequence(2, 5).toString().matches("[0-9]+")) {
+							throw new idFormatException();
+						}
+					} catch (idFormatException e) {
+						e.printMessage();
+						i = i + 1;
+						continue;
+					}
+					
+					// Checking for existing id
+					try {
+						for (Person existingPerson : PersonCollection) {
+							if (idInput.compareToIgnoreCase(existingPerson.getId()) == 0) {
+								duplicateID = true;
+								break;
+							}
+						}
+						if (duplicateID) {
+							throw new duplicateIDException();
+						}
+					} catch (duplicateIDException e) {
+						e.printMessage();
+						i = i + 1;
+						continue;
+					}
+					System.out.print("Department: ");
+					departmentInput = inputScanner.nextLine();
+					
+					// Checking for invalid department input
+					try {
+						if (departmentInput.compareToIgnoreCase("Mathematics") != 0) {
+							if (departmentInput.compareToIgnoreCase("Engineering") != 0) {
+								if (departmentInput.compareToIgnoreCase("English") != 0) {
+									throw new departmentInputException();
+								}
+							}
+						}
+					} catch (departmentInputException e) {
+						e.printMessage();
+						i = i + 1;
+						continue;
+					}
+					System.out.print("Rank: ");
+					rankInput = inputScanner.nextLine();
+					
+					// Checking for invalid rank input
+					try {
+						if (rankInput.compareToIgnoreCase("Professor") != 0) {
+							if (rankInput.compareToIgnoreCase("Adjunct") != 0) {
+								throw new rankInputException();
+							}
+						}
+					} catch (rankInputException e) {
+						e.printMessage();
+						i = i + 1;
+						continue;
+					}
+					
+					PersonCollection.add(new Faculty(fullNameInput, idInput, departmentInput, rankInput));
+					System.out.println("Faculty added!");
+				}
+			} else if (option.compareTo("2") == 0) {
+				
+			} else if (option.compareTo("3") == 0) {
+				
+			} else if (option.compareTo("4") == 0) {
+				
+			} else if (option.compareTo("5") == 0) {
+				
+			} else if (option.compareTo("6") == 0) {
+				
+			} else if (option.compareTo("7") == 0) {
+				
+			} else if (option.compareTo("8") == 0) {
+				
+				exitProgram = true;
+			}
+		}
 	}
-
+	
 }
 
 abstract class Person{
@@ -88,9 +218,9 @@ class Student extends Person{
 	@Override
 	public void print() {
 		System.out.println("Student Name: " + getFullName() + 
-							"\tID: " + getId() + "\nGPA: " + gpa + 
-							"\nCredit Hours: " + creditHours +
-							"\nTuition (after discount if applicable): " + calculateTuition());
+						   "\tID: " + getId() + "\nGPA: " + gpa + 
+						   "\nCredit Hours: " + creditHours +
+						   "\nTuition (after discount if applicable): " + calculateTuition());
 	}
 
 	public double calculateTuition() {
@@ -120,10 +250,10 @@ class Faculty extends Employee{
 	
 	@Override
 	public void print() {
-		System.out.print("Faculty Information\nFull Name: " + getFullName() +
-				 "\nID: " + getId() +
-				 "\nDepartment: " + getDepartment() +
-				 "\nRank: " + rank);
+		System.out.println("Faculty Information\nFull Name: " + getFullName() +
+						 "\nID: " + getId() +
+						 "\nDepartment: " + getDepartment() +
+						 "\nRank: " + rank);
 	}
 }
 
@@ -144,9 +274,37 @@ class Staff extends Employee{
 	
 	@Override
 	public void print() {
-		System.out.print("Staff Information\nFull Name: " + getFullName() +
-				 "\nID: " + getId() +
-				 "\nDepartment: " + getDepartment() +
-				 "\nStatus: " + status);
+		System.out.println("Staff Information\nFull Name: " + getFullName() +
+				 		   "\nID: " + getId() +
+				 		   "\nDepartment: " + getDepartment() +
+				 		   "\nStatus: " + status);
+	}
+}
+
+class idFormatException extends Exception {
+	public void printMessage() {
+		System.out.println("Invalid ID format. Must be LetterLetterDigitDigitDigitDigit");
+		System.out.println("\n\nTry again!");
+	}
+}
+
+class duplicateIDException extends Exception {
+	public void printMessage() {
+		System.out.println("Duplicate ID provided. Must be a new ID");
+		System.out.println("\n\nTry again!");
+	}
+}
+
+class departmentInputException extends Exception {
+	public void printMessage() {
+		System.out.println("Invalid department input. Must be mathematics, engineering, or english");
+		System.out.println("\n\nTry again!");
+	}
+}
+
+class rankInputException extends Exception {
+	public void printMessage() {
+		System.out.println("Invalid rank input. Must be professor or adjunct");
+		System.out.println("\n\nTry again!");
 	}
 }
